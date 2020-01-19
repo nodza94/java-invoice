b402156cf59ae76d63de2250b7d166b82e9cd883
Joanna Nodzynska <jnodzynsk@gmail.com>
2020-01-19 11:51:01 +0100

    Fixed case of negative or zero quantity

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,19 +2,25 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.Collection;
+import java.util.HashMap;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
 public class Invoice {
-	private Collection<Product> products;
+	//private Collection<Product> products;
+	private HashMap <Product, Integer> products;
 
 	public void addProduct(Product product) {
-		products.add(product);
+		products.put(product, 1);
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		for (int i = 0; i < quantity; i++) {
-			products.add(product);
+		//for (int i = 0; i < ; i++) {
+		if (quantity > 0) {
+			products.put(product, quantity);
+		}
+		else {
+			throw new IllegalArgumentException("Quantity can't be negative or zero");
 		}
 	}
 
@@ -23,27 +29,36 @@ public class Invoice {
 
 		if (products != null && products.size() > 0) {
 
-			for (Product product : products) {
+			for (Product product : products.keySet()) {
 
 				sum.add(product.getPrice());
 			}
 			
 		}
 		return sum;
+		
 	}
 
 	public BigDecimal getTax() {
 		BigDecimal sumTax = BigDecimal.ZERO;
 		
 		if (products != null && products.size() > 0) {
-			
+			for (Product product : products.keySet()) {
+
+				sumTax.add(product.getPrice().multiply(product.getTaxPercent()));
+			}
 		}
 		return sumTax;
 	}
 
 	public BigDecimal getTotal() {
 		BigDecimal sumTotal = BigDecimal.ZERO;
-		
+		if (products != null && products.size() > 0) {
+			for (Product product : products.keySet()) {
+
+				sumTotal.add(product.getPrice().multiply(product.getTaxPercent()));
+			}
+			}
 		return sumTotal;
 	}
 }
